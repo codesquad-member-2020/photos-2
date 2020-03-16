3cd3f66bc458716d810bea383049f9fc54e0a526
delma <sdy2856@gmail.com>
2020-03-16 15:01:42 +0900

    Feature: get Photos from Photo library

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     @IBOutlet weak var collectionView: UICollectionView!
 
+    var allPhotos: PHFetchResult<PHAsset>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         collectionView.dataSource = self
         collectionView.delegate = self
+        allPhotos = PHAsset.fetchAssets(with: nil)
         collectionView.reloadData()
     }
 
@@ -26,11 +30,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
 
-        let red: CGFloat = indexPath.item % 2 == 0 ? 0 : 0.8
-        let green: CGFloat = indexPath.item % 3 == 0 ? 0 : 0.8
-        let blue: CGFloat = indexPath.item % 5 == 0 ? 0 : 0.8
-
-        cell.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        let asset = self.allPhotos?[indexPath.item]
 
         return cell
     }
