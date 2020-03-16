ed4f77a30649d86536f825d51153020b43757dd4
delma <sdy2856@gmail.com>
2020-03-16 15:45:06 +0900

    Add: 사진 이미지를 cell에 표시

--- PhotosApp/PhotosApp/CollectionViewCell.swift
+++ PhotosApp/PhotosApp/CollectionViewCell.swift
@@ -10,4 +10,5 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet var image: UIImageView!
 }
--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -11,29 +11,39 @@ import Photos
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     @IBOutlet weak var collectionView: UICollectionView!
-
+    
     var allPhotos: PHFetchResult<PHAsset>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         collectionView.dataSource = self
         collectionView.delegate = self
         allPhotos = PHAsset.fetchAssets(with: nil)
         collectionView.reloadData()
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 40
+        return 2
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
-
-        let asset = self.allPhotos?[indexPath.item]
-
+        
+        
         return cell
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        let options = PHImageRequestOptions()
+        options.isNetworkAccessAllowed = true
+        options.isSynchronous = false
+        PHImageManager.default().requestImage(for: (allPhotos?[indexPath.row])!, targetSize: CGSize(width: 100, height: 100), contentMode: PHImageContentMode.aspectFill, options: options) { (image, info) -> Void in
+            if (image != nil) {
+                (cell as! CollectionViewCell).image.image = image
+            }
+        }
+    }
+    
 }
 
